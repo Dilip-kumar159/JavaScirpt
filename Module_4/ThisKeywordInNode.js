b880b972e36b5bd41f0a64efe56219de95004456
Dilip <dilip45377@gmail.com>
2022-10-26 11:35:19 +0530

    this keyword in node.js with non-strict mode

---
+++ Module_4/ThisKeywordInNode.js
@@ -0,0 +1,35 @@
+// This keyword in Node Js with non-strict mode
+
+// console with this 
+console.log(this); // It gives empty object.
+
+// this keyword in function
+function print(){
+    console.log(this);
+}
+// print()  gives the gobal object
+
+// this keyword in object-> function
+
+let obj = {
+    name : 'Dilip',
+    age : 20,
+    fun : function solve(){
+        console.log(this);
+    }
+}
+// obj.fun(); // returns the whole object itself
+
+// this keyword in object -> function -> function
+
+let obj2 = {
+    name : 'Dilip',
+    age : 20,
+    fun : function solve(){
+            function add(){
+                console.log(this)
+            }
+        add()
+    }
+}
+obj2.fun() // returns the global object 
