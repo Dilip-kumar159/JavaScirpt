b73cb898aa1e3daac87370b4b323139f36c98d4d
Dilip <dilip45377@gmail.com>
2022-10-27 14:14:57 +0530

    read the new dummytext file in Asynchronously

---
+++ Module_5/FileReadAsynchronously.js
@@ -0,0 +1,34 @@
+// import the fs module to use
+const fs = require('fs');
+
+console.log('Start Line');
+
+// let readFile = fs.readFileSync('dummyFile.txt');  This is Synchronously
+
+// let readFile1 = fs.readFileSync('dummyFile1.txt');
+
+// console.log(`${readFile}`);
+
+// console.log(`${readFile1}`);
+
+// Asynchoronously 
+
+fs.readFile('dummyFile.txt', callbackFun); // the second argument is callback function
+
+function callbackFun(err, data){
+    if(err){
+        console.log(err);
+    }
+    console.log('first file ----> '+ data);
+}
+
+fs.readFile('dummyFile1.txt', callbackFun1);
+
+function callbackFun1(err, data){  // In case if there is no file present it throws an error.
+    if(err){
+        console.log(err);
+    }
+    console.log('second file ---> ' + data);
+}
+
+console.log('End Line')
